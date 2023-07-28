5e3e37603fbc2c7a1e5a35c42aff1a79b537a8a2
fernando <fernando.ojeda@sip.cl>
2023-07-28 10:57:59 -0400

    van los arreglos de GUI

--- server.py
+++ server.py
@@ -1,12 +1,9 @@
-# App config
-from app import app
-
-# Controllers
 from app.controllers import login
 from app.controllers import base
 from app.controllers import prestamos
 from app.controllers import familias
 
-
+#App config
+from app import app
 if __name__=="__main__":
-    app.run(debug=True)   
+    app.run(debug=True)
